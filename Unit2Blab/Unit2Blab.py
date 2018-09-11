9a7e84205a9afd2d70419f0334c1d7cbe3424eb3
RDL2020 <Lenr@bellarmineprep.org>
2018-09-11 09:03:19 -0700

    Unit2Blab from Bellarmine.

---
+++ Unit2Blab/Unit2Blab.py
@@ -0,0 +1,23 @@
+numGrade=int(input('Enter a grade'))
+print(numGrade)
+if numGrade>=90:
+    print('Yahoo you got an A!')
+elif numGrade>=80:
+    print('Good you got a B!')
+elif numGrade>=70:
+    print('Okay you got a C!')
+else:
+    print('You better study harder.')
+
+userInput=(input('What is your favorite color?'))
+print(userInput)
+if userInput==('blue'):
+    print('The sky is blue.')
+elif userInput==('green'):
+    print('The forest is green.')
+elif userInput==('red'):
+    print('I love red.')
+elif userInput==('yellow'):
+    print('Yuck yellow. ')
+else:
+    print('I am a dumb computer-I only know four colors.')
