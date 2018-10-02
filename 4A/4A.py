665c32d62f24ad9451f788265baddee2184d4fe3
RDL2020 <Lenr@bellarmineprep.org>
2018-10-02 09:13:06 -0700

    unit4ALab first very very rough draft from Bellarmine.

---
+++ 4A/4A.py
@@ -0,0 +1,18 @@
+def main():
+    myStringDeVoweled = noVowels()
+    print (myStringDeVoweled)
+
+
+def noVowels():
+    myString = 'how to do this'
+    myStringDeVoweled = ''
+    for vowels in myString:
+        if vowels != 'a' or vowels != 'e' or vowels != 'i' or vowels != 'o' or vowels != 'u':
+
+
+
+
+
+
+
+main()
