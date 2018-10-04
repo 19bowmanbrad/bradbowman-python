8aad1ffcac58e42d620445ec9d43bfa6bbb7625a
RDL2020 <Lenr@bellarmineprep.org>
2018-10-04 09:07:34 -0700

    unit4ALab deVowel part DONE. From Bellarmine.

--- 4A/4A.py
+++ 4A/4A.py
@@ -1,13 +1,21 @@
 def main():
-    myStringDeVoweled = noVowels()
-    print (myStringDeVoweled)
+    myString = str(input('Say something!'))
+    print('Your string is- ' + myString)
+    print ('Your string without vowels is- ' + noVowels(myString))
 
 
-def noVowels():
-    myString = 'how to do this'
-    myStringDeVoweled = ''
-    for vowels in myString:
-        if vowels != 'a' or vowels != 'e' or vowels != 'i' or vowels != 'o' or vowels != 'u':
+
+def noVowels(myString):
+    myString2 = myString
+    myStringDeVoweled = ""
+    for vowels in myString2:
+        if (vowels != "a" and vowels != "e" and vowels != "i" and vowels != "o" and vowels != "u" and vowels != "A" and vowels != "E" and vowels != "I" and vowels != "O" and vowels != "U" ):
+            myStringDeVoweled = myStringDeVoweled + vowels
+
+    return (myStringDeVoweled)
+
+
+
 
 
 
