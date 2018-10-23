77435f5ea29c792a332700af360e78365bc06085
RDL2020 <Lenr@bellarmineprep.org>
2018-10-23 09:01:19 -0700

    Untit4DLab final from Bellarmine.

--- unit4DLab/unit4DLab.py
+++ unit4DLab/unit4DLab.py
@@ -2,11 +2,9 @@ def main():
     shoppingCart = [['tooth paste', 'q-tips', 'milk'], ['milk', 'candy', 'apples'], ['paper', 'pencils', 'q-tips']]
     allItems = allInOne(shoppingCart)
     print(allItems)
-
-
-
+    numQTips = countQTips(shoppingCart)
+    print('There are ' + str(numQTips) + ' q-tips in the shopping cart.')
 #-------------------------------------------------
-
 def allInOne(myShoppingCart):
     print('allInOne')
     allItem = []
@@ -19,30 +17,15 @@ def allInOne(myShoppingCart):
     for x in allItem:
         if x not in newList:
             newList.append(x)
-
-
     return newList
-
 #--------------------------------------------------
-
 def countQTips(myShoppingCart):
     print('countQTips')
-
-
+    numTips = 0
+    for item in myShoppingCart:
+        for x in item:
+            if x == 'q-tips':
+                numTips += 1
+    return numTips
 #---------------------------------------------------
-# challenge
-
-def drinkMoreMilk(myShoppingCart):
-    print('drinkMoreMilk')
-
-
-#---------------------------------------------------
-# challenge
-
-def mooseCookie(myShoppingCart):
-    print('mooseCookie')
-
-
-#----------------------------------------------------
-
 main()
