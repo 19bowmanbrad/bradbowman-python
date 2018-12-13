c1ebc95fa896104f7ff4b8791eaca88a96b38615
19bowmanbrad <19bowmanbradley@bprep.org>
2018-12-13 09:19:08 -0800

    workSheetFourTwo draft two.

--- workSheetFourTwo/src/workSheetFourTwo.java
+++ workSheetFourTwo/src/workSheetFourTwo.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
+
 public class workSheetFourTwo {
     public static void main(String [] args){
         //1a
-//        14
-//        6
-//        2200
-//        98
+        System.out.println("14");
+        System.out.println("6");
+        System.out.println("2200");
+        System.out.println("98");
+        //1b
+        System.out.println("[11, 4, 99, 2, 44, 0, 0, 0]");
+        //1c
+        System.out.println("[3, 3, 0, 0, 6, 9, 0, -18]");
+        //1d
+        int [] array1 = {1,2,3,4};
+        forEachMax(array1);
+    }
+        //1d
+    public static void forEachMax(int[] array){
+        for(int x: array){
+            if ()
+        }
     }
 }
