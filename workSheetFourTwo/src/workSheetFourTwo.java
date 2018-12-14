223ccf49dfb0f96d33126e8e674f33dc9d165468
19bowmanbrad <19bowmanbradley@bprep.org>
2018-12-14 08:58:35 -0800

    workSheetFourTwo draft three from Bellarmine.

--- workSheetFourTwo/src/workSheetFourTwo.java
+++ workSheetFourTwo/src/workSheetFourTwo.java
@@ -12,13 +12,39 @@ public class workSheetFourTwo {
         //1c
         System.out.println("[3, 3, 0, 0, 6, 9, 0, -18]");
         //1d
-        int [] array1 = {1,2,3,4};
-        forEachMax(array1);
+        int [] array1 = {5, 1, 8, 4, 1};
+        int maxNum = forEachMax(array1);
+        System.out.println(maxNum);
+        //1e
+        int [] array2 = {10, -2, 4, -4, 9, -5, 19, -7, 39, -1};
+        double avgNum = average(array2);
+        System.out.println(avgNum);
+        //2a
+        int[] doubleArray = doubleValues(array1);
+        System.out.println(Arrays.toString(doubleArray));
     }
         //1d
-    public static void forEachMax(int[] array){
+    public static int forEachMax(int[] array){
+        int maxNum = 0;
         for(int x: array){
-            if ()
+            if (x > maxNum){
+                maxNum = x;
+            }
         }
+        return maxNum;
+    }
+        //1e
+    public static double average(int [] array){
+        double sum = 0;
+        for (int x: array){
+            sum += x;
+        }
+        return (sum/ array.length);
+    }
+    public static int[] doubleValues(int [] array){
+        for (int x = 0; x < array.length; x ++){
+                array[x] *= 2;
+        }
+        return array;
     }
 }
